8787078de88e41b2a3931e7c3f1c4b0f8974a5ac
cv0cv0 <linux2.0@foxmail.com>
2019-01-11 11:20:33 +0800

    Initial commit

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.kt
@@ -4,25 +4,31 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
 class Practice13GetTextBoundsView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
     private val paint1 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style=Paint.Style.STROKE
-        strokeWidth=20f
-        color=Color.parseColor("#E91E63")
+        style = Paint.Style.STROKE
+        strokeWidth = 20f
+        color = Color.parseColor("#E91E63")
     }
-    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply { textSize=160f }
+    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply { textSize = 160f }
 
-    private val text1 = "A"
-    private val text2 = "a"
-    private val text3 = "J"
-    private val text4 = "j"
-    private val text5 = "Â"
-    private val text6 = "â"
+    private val texts = arrayOf("A", "a", "J", "j", "Â", "â")
+    private val offsets = FloatArray(texts.size)
+
     private val top = 200f
     private val bottom = 400f
+
+    init {
+        val rect = Rect()
+        repeat(texts.size) {
+            paint2.getTextBounds(texts[it], 0, texts[it].length, rect)
+            offsets[it] = -(rect.top + rect.bottom) / 2f
+        }
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -32,11 +38,8 @@ class Practice13GetTextBoundsView(context: Context, attrs: AttributeSet?) : View
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
         val middle = (top + bottom) / 2
-        canvas.drawText(text1, 100f, middle, paint2)
-        canvas.drawText(text2, 200f, middle, paint2)
-        canvas.drawText(text3, 300f, middle, paint2)
-        canvas.drawText(text4, 400f, middle, paint2)
-        canvas.drawText(text5, 500f, middle, paint2)
-        canvas.drawText(text6, 600f, middle, paint2)
+        repeat(texts.size) {
+            canvas.drawText(texts[it], (it + 1) * 100f, middle + offsets[it], paint2)
+        }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.kt
@@ -7,41 +7,33 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class Practice14GetFontMetricsView : View {
-    internal var paint1 = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var paint2 = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var texts = arrayOf("A", "a", "J", "j", "Â", "â")
-    internal var top = 200
-    internal var bottom = 400
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
-
-    init {
-        paint1.style = Paint.Style.STROKE
-        paint1.strokeWidth = 20f
-        paint1.color = Color.parseColor("#E91E63")
-        paint2.textSize = 160f
+class Practice14GetFontMetricsView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint1 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = 20f
+        color = Color.parseColor("#E91E63")
     }
+    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply { textSize = 160f }
+
+    private val texts = arrayOf("A", "a", "J", "j", "Â", "â")
+    private val offset by lazy(LazyThreadSafetyMode.NONE) {
+        val metrics = paint2.fontMetrics
+        -(metrics.ascent + metrics.descent) / 2f
+    }
+
+    private val top = 200f
+    private val bottom = 400f
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        canvas.drawRect(50f, top.toFloat(), (width - 50).toFloat(), bottom.toFloat(), paint1)
+        canvas.drawRect(50f, top, width - 50f, bottom, paint1)
 
         // 使用 Paint.getFontMetrics() 计算出文字的显示区域
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
-
         val middle = (top + bottom) / 2
-        canvas.drawText(texts[0], 100f, middle.toFloat(), paint2)
-        canvas.drawText(texts[1], 200f, middle.toFloat(), paint2)
-        canvas.drawText(texts[2], 300f, middle.toFloat(), paint2)
-        canvas.drawText(texts[3], 400f, middle.toFloat(), paint2)
-        canvas.drawText(texts[4], 500f, middle.toFloat(), paint2)
-        canvas.drawText(texts[5], 600f, middle.toFloat(), paint2)
+        repeat(texts.size) {
+            canvas.drawText(texts[it], (it + 1) * 100f, middle + offset, paint2)
+        }
     }
 }
